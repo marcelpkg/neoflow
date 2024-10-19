b2f841417bc2d5944fd68e7935707795e07fd8e1
marcelpkg <marcel.serdak@gmail.com>
2024-10-19 16:53:30 +0100

    added denols but forgot to remove ts_ls oops

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -44,7 +44,6 @@ return {
                   }
                 })
             })
-            lspconfig.ts_ls.setup({})
             lspconfig.yamlls.setup({})
 
             lspconfig.denols.setup({
